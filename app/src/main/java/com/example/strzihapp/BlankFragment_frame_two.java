3607b4ff27c29a0f20c89f9ef393c50756265068
Daniil <strigius2013@gmail.com>
2024-03-12 22:39:13 +0300

    lab7 работает +

---
+++ app/src/main/java/com/example/strzihapp/BlankFragment_frame_two.java
@@ -0,0 +1,21 @@
+package com.example.strzihapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class BlankFragment_frame_two extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_blank_frame_two, container, false);
+    }
+}
