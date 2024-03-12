ca59d0791e9ff833782d5b518a38665d629a8da6
Daniil <strigius2013@gmail.com>
2024-03-12 14:13:32 +0300

    lab7

--- app/src/main/java/com/example/strzihapp/MainActivityRecycler.java
+++ app/src/main/java/com/example/strzihapp/MainActivityRecycler.java
@@ -1,8 +1,10 @@
 package com.example.strzihapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 public class MainActivityRecycler extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class MainActivityRecycler extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_recycler);
+
+        FrameLayout frameLayout = findViewById(R.id.frame_one);
+        ItemFragment_note blankFragment = new ItemFragment_note();
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.frame_one, blankFragment);
+        ft.commit();
+        //fr2();
     }
+
+//    public void fr2()
+//    {
+//        FrameLayout frameLayout2 = findViewById(R.id.frame_two);
+//        ItemFragment_note blankFragment2 = new ItemFragment_note();
+//        FragmentTransaction ft2 = getSupportFragmentManager().beginTransaction();
+//        ft2.replace(R.id.frame_two, blankFragment2);
+//        ft2.commit();
+//    }
 }
