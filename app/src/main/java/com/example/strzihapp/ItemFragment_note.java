c580773a53182efa94c002891cdb6d66a004e3fc
Daniil <strigius2013@gmail.com>
2024-03-12 14:21:26 +0300

    lab7

--- app/src/main/java/com/example/strzihapp/ItemFragment_note.java
+++ app/src/main/java/com/example/strzihapp/ItemFragment_note.java
@@ -1,6 +1,7 @@
 package com.example.strzihapp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -11,6 +12,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -44,6 +46,30 @@ public class ItemFragment_note extends Fragment {
         recyclerView = view.findViewById(R.id.list);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
+        NotesAdapter adapter = new NotesAdapter(requireContext(), notes);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                TaskModel selectedNote = notes.get(position);
+                Intent intent = new Intent(requireContext(), MainActivity3.class);
+                intent.putExtra("name", selectedNote.getName());
+                intent.putExtra("desc", selectedNote.getDescription());
+                //startActivity(intent);
+
+                Bundle bundle = new Bundle();
+                bundle.putString("Key", selectedNote.getName());
+                BlankFragment2 blankFragment2 = new BlankFragment2();
+                blankFragment2.setArguments(bundle);
+                ((MainActivity2) requireActivity()).sharedString = selectedNote.getName();
+                ((MainActivity2) requireActivity()).fr2();
+
+//                nameView = view.findViewById(R.id.textViewName);
+//                descView = view.findViewById(R.id.textView2Desc);
+//                nameView.setText(selectedNote.getName());
+//                descView.setText(selectedNote.getDescription());
+            }
+        });
 
 
 
