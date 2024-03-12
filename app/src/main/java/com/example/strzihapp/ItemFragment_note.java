12117b54b2ae432585db762791ef40c7d1158105
Daniil <strigius2013@gmail.com>
2024-03-12 13:38:32 +0300

    lab7

---
+++ app/src/main/java/com/example/strzihapp/ItemFragment_note.java
@@ -0,0 +1,29 @@
+package com.example.strzihapp;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.strzihapp.placeholder.PlaceholderContent;
+
+
+public class ItemFragment_note extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_item_note_list, container, false);
+
+
+        return view;
+    }
+}
